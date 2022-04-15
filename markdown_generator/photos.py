48b33b61275f3260eb748a60b9b3aca16b33d910
kelihan <101952384+kelihan@users.noreply.github.com>
2022-04-15 18:06:55 +0800

    Create photos.py

---
+++ markdown_generator/photos.py
@@ -0,0 +1,31 @@
+import os
+targetPath=os.path.join(os.path.split(os.path.realpath(__file__))[0], '../_albums')
+dirPath=os.path.join(os.path.split(os.path.realpath(__file__))[0], '../keli_photo')
+dirNames=['ID', 'feature', 'group', 'life', 'other', 'work']
+dirMap={
+    'ID':'证件照',
+    'feature':'feature',
+    'group':'集体合影',
+    'life':'生活照',
+    'other':'其他',
+    'work':'工作照',
+}
+for dirName in dirNames:
+    print(dirPath+'/'+dirName)
+    for root,d, files in os.walk(dirPath+'/'+dirName):
+        print(files)
+        md_filename=dirName+'.md'
+        md=''
+        md += '---\n'
+        md += 'title: "'+dirMap[dirName]+'"\n'
+        md += 'collection: albums\n'
+        md += 'permalink: /album/'+dirName+'\n'
+        md += '---\n'
+
+        for file in files:
+            md += '<a href="'+file+'"><img src="'+file+'" height="100"></a>\n'
+        
+        md_filename = os.path.join(targetPath, md_filename)
+        print(md_filename)
+        with open(md_filename, 'w') as f:
+            f.write(md)
