9fa04b749ed4da493b8dd9749d40c0366e842860
kelihan <101952384+kelihan@users.noreply.github.com>
2022-05-06 15:08:08 -0400

    Update photos.py

--- markdown_generator/photos.py
+++ markdown_generator/photos.py
@@ -1,4 +1,5 @@
 import os
+from os.path import isfile, join
 targetPath=os.path.join(os.path.split(os.path.realpath(__file__))[0], '../_albums')
 dirPath=os.path.join(os.path.split(os.path.realpath(__file__))[0], '../keli_photo')
 dirNames=['ID', 'group', 'life', 'others', 'work']
@@ -10,6 +11,8 @@ dirMap={
     'others':'其他',
     'work':'工作照',
 }
+
+pdffiles = [f[:-3] for f in listdir('../docs/') if isfile(join('../docs/', f))]
 for dirName in dirNames:
     print(dirPath+'/'+dirName)
     for root,d, files in os.walk(dirPath+'/'+dirName):
@@ -26,6 +29,8 @@ for dirName in dirNames:
         for file in files:
             if dirName == 'ID':
                 md += '<a href="../keli_photo/'+ dirName + '/'+file+'"><img src="../keli_photo/'+ dirName + '/' +file+'" height="100"></a>\n'
+            elif file[:-3] in pdffiles:
+                md += '<figure><a href="../keli_photo/'+ dirName + '/'+file+'"><img class="gallery-img" src="../keli_photo/'+ dirName + '/' +file+'" width="24%"></a><figcaption align = "center"><b>报道</b></figcaption></figure>\n'
             else:
                 md += '<a href="../keli_photo/'+ dirName + '/'+file+'"><img class="gallery-img" src="../keli_photo/'+ dirName + '/' +file+'" width="24%"></a>\n'
         
